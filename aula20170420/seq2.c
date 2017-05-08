589ea1794d7ff9ae701e15ae92312a596286fa88
thael99 <thaelzaruz@gmail.com>
2017-05-08 01:33:14 -0300

    Create seq2.c

---
+++ aula20170420/seq2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	double a, b, c, d, i;
+	printf ("Digite a base e a altura do paralelogramo: ");
+	scanf ("%lf %lf", &a, &b);
+	printf ("A area eh %g", a*b);
+	return 0;
+}
