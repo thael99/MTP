143d186c981ee06a5ab976ed52e960a55db5b493
thael99 <thaelzaruz@gmail.com>
2017-05-08 01:32:25 -0300

    Create sec1.c

---
+++ aula20170420/sec1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	double a, b, c, d;
+	printf ("Digite o lado do quadrado: \n");
+	scanf ("%lf", &a);
+	printf  ("A area do quadrado eh %g\n", a*a);
+	return 0;
+}
