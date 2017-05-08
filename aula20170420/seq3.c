903a6ba019a95f9162df80814dce37c1130606a2
thael99 <thaelzaruz@gmail.com>
2017-05-08 01:33:39 -0300

    Create seq3.c

---
+++ aula20170420/seq3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	double a, b, c, d, e, i;
+	scanf ("%lf %lf %lf %lf %lf", &a, &b, &c, &d, &e);
+	printf ("A nota eh %g", ((a+b+c)*2+d*3+e*5)/14);
+
+	return 0;
+}
